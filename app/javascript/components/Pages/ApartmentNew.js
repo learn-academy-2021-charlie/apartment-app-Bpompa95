7c52f6f77db96c8eb26ea9b35c12253e77fa4dc2
Bpompa95 <brianpompa02@gmail.com>
2021-09-01 14:12:30 -0700

    reverted history to working state

---
+++ app/javascript/components/Pages/ApartmentNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ApartmentNew extends Component {
+  render() {
+    return (
+      <>
+        <h3>Apartment New</h3>
+      </>
+    )
+  }
+}
+export default ApartmentNew
