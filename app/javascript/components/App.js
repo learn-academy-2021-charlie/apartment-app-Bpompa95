d09c128ab1cb3d416a4b2d5674c964ff15bf80ea
Bpompa95 <brianpompa02@gmail.com>
2021-09-01 11:32:35 -0700

    i made my 3 folders and headers and footers

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,30 +1,32 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, { Component } from 'react'
+import Header from './components/Header'
+import ApartmentIndex from './pages/ApartmentIndex'
 
-class App extends React.Component {
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+class App extends Component {
   render() {
     const {
       logged_in,
-      current_user,
-      new_user_route,
       sign_in_route,
       sign_out_route
     } = this.props
     return (
-      <React.Fragment>
-        { logged_in &&
-          <div>
-            <a href={sign_out_route }>Sign Out</a>
-          </div>
-        }
-        { !logged_in &&
-          <div>
-            <a href={ sign_in_route }>Sign In</a>
-          </div>
-        }
-      </React.Fragment>
+      <Router>
+        <Header
+          logged_in={logged_in}
+          sign_in_route={sign_in_route}
+          sign_out_route={sign_out_route}
+        />
+        <Switch>
+          <Route path="/apartmentIndex" component={ApartmentIndex}/>
+        </Switch>
+      </Router>
     )
   }
 }
-
 export default App
